6762457527ea10d0fd0e878a0f7ec8b16e411f19
chowwy-wm19 <112604998+chowwy-wm19@users.noreply.github.com>
2022-09-01 12:46:44 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cotehfneowq8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chowwengyong-employee"
+customregion = "us-east-1"
